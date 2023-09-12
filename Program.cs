b22ce1dc9011ca4fd60e7cc7135d5f4734055e40
Somnath Bera <somnathbera1919@gmail.com>
2023-09-12 13:01:50 +0530

    added secure page which can be accessed only after authentication and when not authenticated login page redirection

--- Program.cs
+++ Program.cs
@@ -3,6 +3,24 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 
+//builder.Services.AddRazorPages(options =>
+//{
+//    options.Conventions.AddPageRoute("/Pages/Account/Login", "/account/login"); // Map /account/login to your login page.
+//});
+
+
+//auth scheme configuration
+builder.Services.AddAuthentication("cookie")
+    .AddCookie("cookie", o =>
+    {
+        o.Cookie.Name = "demo";
+        o.ExpireTimeSpan = TimeSpan.FromDays(1);
+
+        //redirection path when challenged
+        o.LoginPath = "/account/login"; 
+
+    });
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -18,8 +36,14 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+//authentication middleware
+app.UseAuthentication();
+
+// authorization middleware
 app.UseAuthorization();
 
 app.MapRazorPages();
+
+
 
 app.Run();
