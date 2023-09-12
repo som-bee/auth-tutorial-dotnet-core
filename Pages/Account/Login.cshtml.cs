4ff5c5214a7e0034fb1518fd2b7aa1dca6488dbc
Somnath Bera <somnathbera1919@gmail.com>
2023-09-12 14:21:21 +0530

    login functionality using cookies

--- Pages/Account/Login.cshtml.cs
+++ Pages/Account/Login.cshtml.cs
@@ -1,12 +1,45 @@
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Security.Claims;
 
 namespace AuthenticationTutorial.Pages.Account
 {
     public class LoginModel : PageModel
     {
+        [BindProperty]
+        public String Username { get; set; }
+
+        [BindProperty]
+        public string Password { get; set; }
+
+        [BindProperty(SupportsGet =true)]
+        public String ReturnUrl { get; set; }
         public void OnGet()
         {
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            if (!string.IsNullOrWhiteSpace(Username) && Username == Password)
+            {
+                var claims = new List<Claim>
+                {
+                    new ("sub","123"),
+                    new("username","sombee"),
+                    new("role","admin")
+                };
+
+                var claimsId = new ClaimsIdentity(claims, "pwd", "username","role");
+
+                var claimsPrin = new ClaimsPrincipal(claimsId);
+
+                await HttpContext.SignInAsync(claimsPrin);
+
+                return LocalRedirect(ReturnUrl);
+
+            }
+            return Page();
         }
     }
 }
